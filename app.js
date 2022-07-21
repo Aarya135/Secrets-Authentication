d2016d9429281ec9250fbe374d18127079bb788f
Aarya135 <apathania_be19@thapar.edu>
2022-07-22 00:00:14 +0530

    Callback url changed

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://https://fierce-bayou-16578.herokuapp.com/auth/google/secrets",
+    callbackURL: "http://fierce-bayou-16578.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
